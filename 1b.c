5ed5ce750dfc1e664aa50d48f3d2c6adf1628d87
David Estes McKnight <belmont95@live.com>
2017-11-07 19:59:36 -0600

    Added method declaration to top of 1b.c

--- 1b.c
+++ 1b.c
@@ -32,6 +32,8 @@ C
 #include <unistd.h>
 #include <sys/wait.h>
 
+void bashLaunch(char* command);
+
 #define MAX_BUFFER 1024 // max line buffer
 #define MAX_ARGS 64 // max # args
 #define SEPARATORS " \t\n" // token sparators
